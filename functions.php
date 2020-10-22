fb6b1eb7670d75edce14d5aadcdeba644ecd6b9b
Linn <54114194+LinnJosefsson@users.noreply.github.com>
2020-10-22 20:27:25 +0200

    rättade till och gjorde snyggt

--- functions.php
+++ functions.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 require __DIR__ . ('/data.php');
 
-$year = rand(1, 2020);
-$month = rand(1, 12);
-$day = rand(1, 28);
+
 function randomDate($day, $month, $year)
 {
+
+    $day = rand(1, 28);
+    $month = rand(1, 12);
+    $year = rand(1, 2020);
+
     return $day . "-" . $month . "-" . $year;
 }
-echo randomDate($day, $month, $year);
